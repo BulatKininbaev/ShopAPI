c44d91343bec09b9c3df9e0d2c720a22737a6036
BulatKinibaev <bkenay@gmail.com>
2025-10-21 02:35:42 +0300

    First commit

--- shop_store/urls.py
+++ shop_store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about,products
 
 urlpatterns = [
-    path('', index),
-    path('about/', about),
+    path('index', index,name='index'),
+    path('products/<int:category_id>', products,name='products'),
+    path('about/', about, name='about'),
 ]
--- shop_store/views.py
+++ shop_store/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category,Product
 
 
 def index(request):
-    """Главная страница."""
-    return HttpResponse('<h1>Hello, магазин продаж?</h1>')
+    """Главная страница. С возвратом каталога товаров"""
+    category = Category.objects.all()
+    context = {
+        'content': category,
+    }
+    return render(request, 'index.html', context=context)
+
+def products(request,category_id):
+    """Товары по выбранному каталогу"""
+    prod = Product.objects.filter(category_id=category_id)
+    cat_name = Category.objects.get(pk=category_id)
+    context = {
+        'category_name': cat_name,
+        'content': prod,
+    }
+    return render(request, 'products.html', context=context)
 
 
 def about(request):
     """Страница о нас."""
-    return HttpResponse('<h1>О нас</h1>')
+    return render(request, 'about.html')
 
